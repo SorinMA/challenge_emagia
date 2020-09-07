786def80e4eb77a7e405bb37b07933127b4ce340
SorinMA <sorinmartinescu9dev@gmail.com>
2020-09-07 19:35:59 +0300

    sad pepe after 5 h finished the setup, pray for my ass bby :((

---
+++ gameEmagia/src/app/models/Orderus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace appemag\app\models;
+class Orderus {
+    public $name;
+
+    function get_hello() {
+        return "hello";
+    }
+}
+?>
---
+++ gameEmagia/src/index.php
@@ -0,0 +1,8 @@
+<?php
+require "../vendor/autoload.php";
+use appemag\app\models\Orderus;
+
+$o = new Orderus();
+
+echo $o::get_hello();
+?>
